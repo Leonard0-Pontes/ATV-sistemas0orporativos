b452b97df53942633a61bae6cbf659e2ff9b99b2
luciano.farias <luciano.cnrn@gmail.com>
2026-07-10 10:13:25 -0300

    atualiza repositorio

--- src/estado/estado.module.ts
+++ src/estado/estado.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { EstadoController } from './estado.controller';
 
-@Module({})
+@Module({
+  controllers: [EstadoController]
+})
 export class EstadoModule {}
