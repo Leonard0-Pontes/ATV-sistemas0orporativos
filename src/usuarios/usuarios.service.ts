9c00bcc26018fff3db4b590b2c85a8b1eedf59de
Leonard0-Pontes <leoptsrn@gmail.com>
2026-09-02 17:25:21 -0300

    alteraçoes da atividade-2

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -35,11 +35,22 @@ export class UsuariosService {
     },
     {
     id: 3,
-    nome: 'Bruno Silva',
-    email: 'bruno@empresa.com',
+    nome: 'leonardo pontes',
+    email: 'leonardo.pontes@escolar.ifrn.com',
+    //2025103806005
     senhaHash:
-      '$2b$12$5S9LDbR3FznMAsZY5P..2OKE932dOHeVvGrmlfklgquClbkKgUidC',
-    papel: 'solicitante',
+      '$2b$12$zqjInxEzlLTqQCZs2DJDo.EiJItxD8A8GT4xA63fkjsaWsiFo6s8e',
+    papel: 'gestor',
+    ativo: true,
+    },
+    {
+    id: 4,
+    nome: 'leonardo pontes',
+    email: 'leonardo.pontes@escolar.ifrn.com',
+    // 50006083015202
+    senhaHash:
+      '$2b$12$dYtU26UsxRIl6hWjOWLDZOSxshsxgX.PP.8vXYL1.wDdSSwCLhNaa',
+    papel: 'gestor',
     ativo: true,
     },
   ];
